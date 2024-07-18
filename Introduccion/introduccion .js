33787984d196c0f58bc42d6a834cda7aefea02e8
ceci <ceciliavcoronel234@gmail.com>
2024-07-17 22:48:41 -0300

    desafio04

---
+++ Introduccion/introduccion .js
@@ -0,0 +1,34 @@
+
+/* 3. Estoy en un archivo de js */
+
+
+/* Variable:espacio de memoria para almacenar datos */
+/*  Con var
+nombres:caracteres/letras
+email:numeros
+ edad:
+ pais:*/
+ /* const representa una constante:no cambia en la ejecucion del programa
+ const DNI = 123456;
+ Esto es una declaracion de una variable y asignacion de un valor
+ 
+ /* 
+
+ Funciones:
+ console.log()
+ prompt es una funcion que lepermite ingresar al usuario un valor
+ confim()
+ es una funcion de tipo booleano.. el usuario solo puede aceptar o cancelar
+
+
+ console.log(nombre + apellido + provincia)
+ console.info
+ console.error
+ console.warn
+
+alert ventana emergente  predefinida */
+let miNombre = 'soy cecilia';
+
+alert(miNombre + ' ' + 'soy una alerta para ti');
+
+
---
+++ Introduccion/matematica.js
@@ -0,0 +1,19 @@
+/* 
+Operadores matematicos
+
+  +  - * % / 
+ 
+  */
+ 15 + 50
+ console.log( 15 + 50);
+
+let numuno = 15;
+let numdos = 50;
+
+let resultado;
+resultado = numuno + numdos;
+console.log(resultado);
+ 
+resultado = numuno - numdos;
+console.log(resultado);
+alert('El resulatdo de la resta: ' + resultado);
